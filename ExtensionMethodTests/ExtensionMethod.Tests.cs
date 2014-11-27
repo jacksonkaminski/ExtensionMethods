23aecbe11bc3a222f563fff1c73345b4c948f924
Jackson Kaminski <nimak247@yahoo.com>
2014-11-27 16:18:38 -0600

    Initial Commit
    initial commit for project

---
+++ ExtensionMethodTests/ExtensionMethod.Tests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using CollectionExtensions;
+
+namespace ExtensionMethodTests
+{
+    [TestFixture]
+    public class TestsCollectionPartitionMethods
+    {
+        int[] _array;
+        int _size = 10;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _array = new int[] { 0, 1, 2, 3 ,4 ,5 ,6 ,7 ,8 ,9 };
+        }
+
+        #region Tests for Slice()
+
+        [Test]
+        public void SliceCanHandleUnderflow()
+        {
+            Assert.AreEqual(new int[0], _array.Slice(-1, 5)); //start underflow
+            Assert.AreEqual(new int[0], _array.Slice(1, -20)); //underflow
+            Assert.AreEqual(new int[0], _array.Slice(0, _size * -1)); //underflow boundry check
+            Assert.AreEqual(new int[0], _array.Slice(1, _size * -1)); //underflow due to position of starting point
+        }
+
+        [Test]
+        public void SliceCanHandleOverflow()
+        {
+            Assert.AreEqual(new int[0], _array.Slice(_size + 1, 5));
+        }
+
+        [Test]
+        public void CanSlice()
+        {
+            Assert.AreEqual(_array.Length, 10);
+            Assert.AreEqual(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }, _array.Slice(0, _size)); //boundary check
+            Assert.AreEqual(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 }, _array.Slice<int>(0, -1)); //boundary and negative end check
+            Assert.AreEqual(new int[] { 2, 3, 4 }, _array.Slice<int>(2, -5)); //negative end check
+            Assert.AreEqual(new int[] { 5, 6, 7, 8 }, _array.Slice<int>(5, 9));
+        }
+
+        #endregion
+    }
+}
---
+++ src/CollectionExtensions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionExtensions
+{
+    public static class CollectionPartitionExtension
+    {
+
+        /// <summary>
+        /// Get the array slice between the two indexes; If overflow or inderflow occurs, 
+        /// will return an empty array of type T. Slice is inclusive for start, exclusive
+        /// for end
+        /// </summary>
+        /// <typeparam name="T">The array type</typeparam>
+        /// <param name="arr">Array to be sliced</param>
+        /// <param name="start">Index at which the slice starts; cannot be negative.</param>
+        /// <param name="end">Endpoint for the slice of the array. Supports negative values</param>
+        /// <returns>Array slice adhereing to the start and ending points, or a strongly
+        /// typed emtpy array if an underflow or overflow situation is encountered</returns>
+        public static T[] Slice<T>(this T[] arr, int start, int end)
+        {
+            int segmentEnd = end;
+            //check for and handle underflow/overflow scenarios
+            if (start < 0 || start > arr.Length)
+            {
+                return new T[0];
+            }
+            if (end > 0 && start > end || end == 0)
+            {
+                return new T[0];
+            }
+
+            if (end < 0)
+            {
+                //end is negative value, so adding will result in subtraction
+                segmentEnd = arr.Length + end;
+
+                //handle scenario where negative end index causes underflow
+                if (segmentEnd < start)
+                {
+                    return new T[0];
+                }
+            }
+
+            int len = segmentEnd - start;
+
+            T[] newArry = new T[len];
+            for (int i = 0; i < len; i++)
+            {
+                newArry[i] = arr[i + start];
+            }
+
+            return newArry;
+        }
+
+
+    }
+}
