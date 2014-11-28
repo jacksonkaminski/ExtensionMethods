883ef502731fe6964d04d392a9e9cf91ad0fd119
Jackson Kaminski <nimak247@yahoo.com>
2014-11-27 18:10:13 -0600

    Added Partition methods

--- ExtensionMethodTests/ExtensionMethod.Tests.cs
+++ ExtensionMethodTests/ExtensionMethod.Tests.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using NUnit.Framework;
 using CollectionExtensions;
 
@@ -13,11 +11,13 @@ namespace ExtensionMethodTests
     {
         int[] _array;
         int _size = 10;
+        List<Person> _persons;
 
         [SetUp]
         public void SetUp()
         {
             _array = new int[] { 0, 1, 2, 3 ,4 ,5 ,6 ,7 ,8 ,9 };
+            _persons = GetPersonList();
         }
 
         #region Tests for Slice()
@@ -45,6 +45,79 @@ namespace ExtensionMethodTests
             Assert.AreEqual(new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8 }, _array.Slice<int>(0, -1)); //boundary and negative end check
             Assert.AreEqual(new int[] { 2, 3, 4 }, _array.Slice<int>(2, -5)); //negative end check
             Assert.AreEqual(new int[] { 5, 6, 7, 8 }, _array.Slice<int>(5, 9));
+        }
+
+        #endregion
+
+        #region Tests for Partition
+
+        [Test]
+        public void CanPartitionPrimitive()
+        {
+            IEnumerable<int>[] partitionedResults1 = _array.Partition<int>(x => x > 5);
+            Assert.AreEqual(partitionedResults1.Length, 2);
+            Assert.AreEqual(partitionedResults1[0].Count(), 4);
+            Assert.AreEqual(partitionedResults1[1].Count(), 6);
+        }
+
+        [Test]
+        public void CanPartitionComplexType()
+        {
+            IEnumerable<Person>[] partitionedResults = _persons.Partition<Person>(x => x.Gender == 'M');
+            Assert.AreEqual(partitionedResults.Length, 2);
+            Assert.AreEqual(partitionedResults[0].Count(), 4);
+            Assert.AreEqual(partitionedResults[1].Count(), 3);
+        }
+
+        [Test]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void CanThrowExceptionOEmptyCollection()
+        {
+            IEnumerable<Person> noPersons = new List<Person>();
+            IEnumerable<Person>[] partitionedResults = 
+                        noPersons.Partition<Person>(x => x.Gender == 'M', OnEmptyCollection.ThrowException);
+
+        }
+
+        [Test]
+        public void CanReturnEmptyCollectionIfEmptyCollection()
+        {
+            IEnumerable<Person> noPersons = new List<Person>();
+            IEnumerable<Person>[] partitionedResults =
+                        noPersons.Partition<Person>(x => x.Gender == 'M', OnEmptyCollection.ReturnEmptyCollections);
+
+            Assert.IsTrue(partitionedResults[0].Count() == 0);
+            Assert.IsTrue(partitionedResults[1].Count() == 0);
+        }
+
+        #endregion
+
+        #region nested class
+
+        class Person
+        {
+            internal string FirstName { get; set; }
+            internal string Lastname { get; set; }
+            internal char Gender { get; set; }
+            internal int Age { get; set; }
+        }
+
+        #endregion
+
+        #region private methods
+
+        private List<Person> GetPersonList()
+        {
+            List<Person> persons = new List<Person>();
+            persons.Add(new Person() { FirstName = "John", Lastname ="Doe", Age = 24, Gender = 'M' });
+            persons.Add(new Person() { FirstName = "Jane", Lastname = "Doe", Age = 30, Gender = 'F' });
+            persons.Add(new Person() { FirstName = "Sally", Lastname = "Murphy", Age = 27, Gender = 'F' });
+            persons.Add(new Person() { FirstName = "Brian", Lastname = "Dorsey", Age = 32, Gender = 'M' });
+            persons.Add(new Person() { FirstName = "William", Lastname = "Fredericks", Age = 50, Gender = 'M' });
+            persons.Add(new Person() { FirstName = "Laura", Lastname = "Appletree", Age = 43, Gender = 'F' });
+            persons.Add(new Person() { FirstName = "Bob", Lastname = "Stevens", Age = 40, Gender = 'M' });
+
+            return persons;
         }
 
         #endregion
--- src/CollectionExtensions.cs
+++ src/CollectionExtensions.cs
@@ -1,14 +1,45 @@
-﻿using System;
+﻿// This project is licensed under The MIT License (MIT)
+//
+// Copyright 2014 Jackson Kaminski
+//
+//	Permission is hereby granted, free of charge, to any person obtaining a copy
+//		of this software and associated documentation files (the "Software"), to deal
+//		in the Software without restriction, including without limitation the rights
+//		to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//		copies of the Software, and to permit persons to whom the Software is
+//		furnished to do so, subject to the following conditions:
+//
+//		The above copyright notice and this permission notice shall be included in
+//		all copies or substantial portions of the Software.
+//
+//		THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//		IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//		FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//		AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//		LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//		OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//		THE SOFTWARE.
+//
+// Please direct questions, patches, and suggestions to the project page at
+
+// @TODO - FINISH PATH
+// https://github.com/...
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CollectionExtensions
 {
+    public enum OnEmptyCollection
+    {
+        ReturnEmptyCollections = 0,
+        ThrowException
+    }
+
     public static class CollectionPartitionExtension
     {
-
+        
         /// <summary>
         /// Get the array slice between the two indexes; If overflow or inderflow occurs, 
         /// will return an empty array of type T. Slice is inclusive for start, exclusive
@@ -56,6 +87,76 @@ namespace CollectionExtensions
             return newArry;
         }
 
+        /// <summary>
+        /// Takes a collection and a predicate function, and returns an array that contains the contents
+        /// of the original collection split into 2 collections. The first collection contains all items
+        /// that returned true from the partition test, the second collection contains all the items that
+        /// returned false from the partition test
+        /// </summary>
+        /// <typeparam name="T">The type for the collection the partition is to be applied to</typeparam>
+        /// <param name="collection">The collection to be partitioned</param>
+        /// <param name="partitionTest">The predicate used to partition the collection's contents</param>
+        public static IEnumerable<T>[] Partition<T>(this IEnumerable<T> collection, Func<T, bool> partitionTest)
+        {
+            return collection.Partition(partitionTest, OnEmptyCollection.ReturnEmptyCollections);
+        }
 
+        /// <summary>
+        ///  Takes a collection and a predicate function, and returns an array that contains the contents
+        /// of the original collection split into 2 collections. The first collection contains all items
+        /// that returned true from the partition test, the second collection contains all the items that
+        /// returned false from the partition test
+        /// </summary>
+        /// <typeparam name="T">The type for the collection the partition is to be applied to</typeparam>
+        /// <param name="collection">The collection to be partitioned</param>
+        /// <param name="partitionTest">The predicate used to partition the collection's contents</param>
+        /// <param name="emptyCollectionStrategy">The strategy to apply if the collection being operated
+        /// on is empty; Options are to return an empty collection or throw an Exception</param>
+        /// <exception cref="InvalidOperationException">If Partition is called on an empty collection when
+        /// the OnEmptyCollection.ThrowException option is selected, then this exception will be raised</exception>
+        public static IEnumerable<T>[] Partition<T>(this IEnumerable<T> collection, Func<T, bool> partitionTest, OnEmptyCollection emptyCollectionStrategy)
+        {
+            IEnumerable<T> passTest = Enumerable.Empty<T>();
+            IEnumerable<T> failTest = Enumerable.Empty<T>();
+
+            if (collection.Count() == 0)
+            {
+                if (emptyCollectionStrategy == OnEmptyCollection.ReturnEmptyCollections)
+                {
+                    return new IEnumerable<T>[] { passTest, failTest }; 
+                }
+
+                throw new InvalidOperationException("Partition attempt failed due to empty collection");
+            }
+
+            foreach (var item in collection)
+            {
+                if (partitionTest(item))
+                {
+                    passTest = passTest.Append<T>(item);
+                }
+                else
+                {
+                    failTest = failTest.Append<T>(item);
+                }
+            }
+
+            return new IEnumerable<T>[] { passTest, failTest };
+        }
+
+        /// <summary>
+        /// Appends a value to an IEnumerable type collection
+        /// </summary>
+        /// <typeparam name="T">The type for the collection</typeparam>
+        /// <param name="enumerable">The collection</param>
+        /// <param name="value">The Value to be appended to the end of the collection</param>
+        public static IEnumerable<T> Append<T>(this IEnumerable<T> enumerable, T value)
+        {
+            foreach (var cur in enumerable)
+            {
+                yield return cur;
+            }
+            yield return value;
+        }
     }
 }
